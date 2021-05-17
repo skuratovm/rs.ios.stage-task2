eaf976d393bdbf9949161c69ca213ccac363aa38
skuratovm <maydibee@gmail.com>
2021-05-17 23:45:02 +0300

    feat:ex 3 done; test passed

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,37 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    
+NSMutableArray *mutableArray = [[NSMutableArray alloc] initWithArray:numbersArray];
+if (numbersArray.count == 0)
+    return @"";
+    
+    
+if (numbersArray.count < 4){
+        for (int i = 0; i < 4 - numbersArray.count; i++){
+            [mutableArray addObject:@0];
+        }
+    }
+    
+bool isNegative = FALSE, isInvalid = FALSE;
+    for (int i = 0; i < 4; i++)
+        if ([mutableArray[i] intValue] < 0){
+            isNegative = true;
+            break;
+        }
+        else if ([mutableArray[i] intValue] > 255){
+            isInvalid = true;
+            break;
+        }
+    if (isNegative)return @"Negative numbers are not valid for input.";
+    if (isInvalid)return @"The numbers in the input array can be in the range from 0 to 255.";
+    
+    
+    NSString *output = [NSString stringWithFormat:@"%d.%d.%d.%d", [mutableArray[0] intValue],
+                        [mutableArray[1] intValue], [mutableArray[2] intValue],
+                        [mutableArray[3] intValue]];
+    
+    return output;
 }
 
 @end
